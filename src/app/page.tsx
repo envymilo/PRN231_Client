794dd9878766905295b991320b05801dbca0dba4
envymilo <nguyenviminhloc@gmail.com>
2024-03-11 20:50:31 +0700

    LoginAndSignUp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,195 @@
+"use client";
+import {
+  Button,
+  Card,
+  CardBody,
+  Input,
+  Link,
+  Tab,
+  Tabs,
+} from "@nextui-org/react";
 import Image from "next/image";
+import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-export default function Home() {
+const Page = () => {
+  const [selected, setSelected] = useState<string | number>("login");
+
+  const signInFormik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string().email("Invalid Email").required("Required"),
+      password: Yup.string().min(6, "At least 6 digits").required("Required"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const signUpFormik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object().shape({
+      name: Yup.string()
+        .min(2, "At least 2")
+        .max(50, "Maximum 50")
+        .required("Required"),
+      email: Yup.string().email("Invalid Email").required("Required"),
+      password: Yup.string().min(6, "At least 6 digits").required("Required"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex flex-col w-full grid h-screen place-content-center">
+      <Card className="max-w-full w-[340px] h-[400px]">
+        <CardBody className="overflow-hidden">
+          <Tabs
+            fullWidth
+            size="md"
+            aria-label="Tabs form"
+            selectedKey={selected}
+            onSelectionChange={setSelected}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <Tab key="login" title="Login">
+              <form
+                className="flex flex-col gap-4"
+                onSubmit={signInFormik.handleSubmit}
+                onReset={signInFormik.handleReset}
+              >
+                <Input
+                  id="email"
+                  value={signInFormik.values.email}
+                  onChange={signInFormik.handleChange}
+                  onBlur={signInFormik.handleBlur}
+                  isInvalid={
+                    !!(signInFormik.touched.email && signInFormik.errors.email)
+                  }
+                  errorMessage={
+                    signInFormik.touched.email && signInFormik.errors.email
+                  }
+                  isRequired
+                  label="Email"
+                  placeholder="Enter your email"
+                  type="email"
+                />
+                <Input
+                  id="password"
+                  value={signInFormik.values.password}
+                  onChange={signInFormik.handleChange}
+                  onBlur={signInFormik.handleBlur}
+                  isInvalid={
+                    !!(
+                      signInFormik.touched.password &&
+                      signInFormik.errors.password
+                    )
+                  }
+                  errorMessage={
+                    signInFormik.touched.password &&
+                    signInFormik.errors.password
+                  }
+                  isRequired
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="password"
+                />
+                <p className="text-center text-small">
+                  Need to create an account?{" "}
+                  <Link size="sm" onPress={() => setSelected("sign-up")}>
+                    Sign up
+                  </Link>
+                </p>
+                <div className="flex gap-2 justify-end">
+                  <Button type="submit" fullWidth color="primary">
+                    Login
+                  </Button>
+                </div>
+              </form>
+            </Tab>
+            <Tab key="sign-up" title="Sign up">
+              <form className="flex flex-col gap-4 h-[300px]">
+                <Input
+                  id="name"
+                  value={signUpFormik.values.name}
+                  onChange={signUpFormik.handleChange}
+                  onBlur={signUpFormik.handleBlur}
+                  isInvalid={
+                    !!(signUpFormik.touched.name && signUpFormik.errors.name)
+                  }
+                  errorMessage={
+                    signUpFormik.touched.name && signUpFormik.errors.name
+                  }
+                  isRequired
+                  label="Name"
+                  placeholder="Enter your name"
+                  type="password"
+                />
+                <Input
+                  id="email"
+                  value={signUpFormik.values.email}
+                  onChange={signUpFormik.handleChange}
+                  onBlur={signUpFormik.handleBlur}
+                  isInvalid={
+                    !!(signUpFormik.touched.email && signUpFormik.errors.email)
+                  }
+                  errorMessage={
+                    signUpFormik.touched.email && signUpFormik.errors.email
+                  }
+                  isRequired
+                  label="Email"
+                  placeholder="Enter your email"
+                  type="email"
+                />
+                <Input
+                  id="password"
+                  value={signUpFormik.values.password}
+                  onChange={signUpFormik.handleChange}
+                  onBlur={signUpFormik.handleBlur}
+                  isInvalid={
+                    !!(
+                      signUpFormik.touched.password &&
+                      signUpFormik.errors.password
+                    )
+                  }
+                  errorMessage={
+                    signUpFormik.touched.password &&
+                    signUpFormik.errors.password
+                  }
+                  isRequired
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="password"
+                />
+                <p className="text-center text-small">
+                  Already have an account?{" "}
+                  <Link size="sm" onPress={() => setSelected("login")}>
+                    Login
+                  </Link>
+                </p>
+                <div className="flex gap-2 justify-end">
+                  <Button fullWidth color="primary">
+                    Sign up
+                  </Button>
+                </div>
+              </form>
+            </Tab>
+          </Tabs>
+        </CardBody>
+      </Card>
+    </div>
   );
-}
+};
+
+//export deffault laf gif
+//export la gif
+
+export default Page;
